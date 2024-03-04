dd92e41e28708b7c4aa39a44e1d538486e9f9a01
Matt Nadareski <mnadareski@outlook.com>
2024-03-03 19:37:05 -0500

    Add hash type enum

---
+++ HashType.cs
@@ -0,0 +1,62 @@
+namespace SabreTools.Hashing
+{
+    /// <summary>
+    /// Available hashing and checksumming types
+    /// </summary>
+    public enum HashType
+    {
+        /// <summary>
+        /// CRC 32-bit checksum
+        /// </summary>
+        CRC32,
+#if NET6_0_OR_GREATER
+
+        /// <summary>
+        /// CRC 64-bit checksum
+        /// </summary>
+        CRC64,
+#endif
+
+        /// <summary>
+        /// MD5 hash
+        /// </summary>
+        MD5,
+
+        /// <summary>
+        /// SHA-1 hash
+        /// </summary>
+        SHA1,
+
+        /// <summary>
+        /// SHA-256 hash
+        /// </summary>
+        SHA256,
+
+        /// <summary>
+        /// SHA-384 hash
+        /// </summary>
+        SHA384,
+
+        /// <summary>
+        /// SHA-512 hash
+        /// </summary>
+        SHA512,
+
+        /// <summary>
+        /// spamsum fuzzy hash
+        /// </summary>
+        SpamSum,
+#if NET6_0_OR_GREATER
+
+        /// <summary>
+        /// xxHash 32-bit hash
+        /// </summary>
+        XxHash32,
+
+        /// <summary>
+        /// xxHash 64-bit hash
+        /// </summary>
+        XxHash64,
+#endif
+    }
+}
