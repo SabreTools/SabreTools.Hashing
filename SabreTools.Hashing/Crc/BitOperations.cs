cb3585ae4910312a83610c6565f63a8b49b6e77a
Matt Nadareski <mnadareski@outlook.com>
2024-11-06 11:56:09 -0500

    Rename parameter

--- SabreTools.Hashing/Crc/BitOperations.cs
+++ SabreTools.Hashing/Crc/BitOperations.cs
@@ -5,10 +5,10 @@ namespace SabreTools.Hashing.Crc
         /// <summary>
         /// Reverse the endianness of a value
         /// </summary>
-        public static ulong ReverseBits(ulong value, int bitLength)
+        public static ulong ReverseBits(ulong value, int bitWidth)
         {
             ulong reverse = 0;
-            for (int i = 0; i < bitLength; i++)
+            for (int i = 0; i < bitWidth; i++)
             {
                 reverse <<= 1;
                 reverse |= value & 1;
@@ -19,12 +19,12 @@ namespace SabreTools.Hashing.Crc
         }
 
         /// <summary>
-        /// Clamp a value to a certain bit length and convert to a byte array
+        /// Clamp a value to a certain bit width and convert to a byte array
         /// </summary>
-        public static byte[] ClampValueToBytes(ulong value, int bitLength)
+        public static byte[] ClampValueToBytes(ulong value, int bitWidth)
         {
-            value &= ulong.MaxValue >> (64 - bitLength);
-            byte[] bytes = new byte[(bitLength + 7) / 8];
+            value &= ulong.MaxValue >> (64 - bitWidth);
+            byte[] bytes = new byte[(bitWidth + 7) / 8];
 
             for (int i = 0; i < bytes.Length; i++)
             {
