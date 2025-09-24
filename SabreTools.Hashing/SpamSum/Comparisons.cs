0c6b5a821aed765b1f400b4e0ad59c014ac6d5ef
HeroponRikiBestest <jojloj3@gmail.com>
2025-09-24 18:23:22 -0400

    Finish cleaning up code.

--- SabreTools.Hashing/SpamSum/Comparisons.cs
+++ SabreTools.Hashing/SpamSum/Comparisons.cs
@@ -3,28 +3,30 @@ using System.Text.RegularExpressions;
 
 namespace SabreTools.Hashing.SpamSum;
 
-// -1 on validity failure, 0 if they're not comparable, score from 0 (least similar) to 100 (most similar) otherwise.
+// 
 internal static class Comparisons
 {
+    /// <summary>
+    /// Compares how similar two SpamSums are to each other. Implements ssdeep's fuzzy_compare.
+    /// </summary>
+    /// <param name="firstHash">First hash to compare</param>
+    /// <param name="secondHash">Second hash to compare</param>
+    /// <returns>-1 on validity failure, 0 if they're not comparable, score from 0 (least similar) to 100 (most similar) otherwise.</returns>
+    /// <link>https://github.com/ssdeep-project/ssdeep/blob/df3b860f8918261b3faeec9c7d2c8a241089e6e6/fuzzy.c#L860</link>
+
     public static int FuzzyCompare(string? firstHash, string? secondHash)
     {
-        const int spamSumLength = 64;
-        uint score = 0;
-        string? stringOneBlockOne, stringOneBlockTwo, stringTwoBlockOne, stringTwoBlockTwo;
-        string? stringOnePrefixOnwards, stringTwoPrefixOnwards;
-        int stringOnePrefixIndex, stringTwoPrefixIndex;
-
         if (firstHash == null || secondHash == null)
             return -1;
 
         // Each SpamSum string starts with its block size before the first semicolon. Verify it's there and return
         // otherwise.
-        stringOnePrefixIndex = firstHash.IndexOf(':');
+        var stringOnePrefixIndex = firstHash.IndexOf(':');
         if (stringOnePrefixIndex == -1)
             return -1;
         if (!uint.TryParse(firstHash.Substring(0, stringOnePrefixIndex), out uint blockSizeOne))
             return -1;
-        stringTwoPrefixIndex = secondHash.IndexOf(':');
+        var stringTwoPrefixIndex = secondHash.IndexOf(':');
         if (stringTwoPrefixIndex == -1)
             return -1;
         if (!uint.TryParse(secondHash.Substring(0, stringTwoPrefixIndex), out uint blockSizeTwo))
@@ -39,8 +41,8 @@ internal static class Comparisons
             return 0;
 
         // Get the spamSum strings starting past the blocksize prefix.
-        stringOnePrefixOnwards = firstHash.Substring(stringOnePrefixIndex + 1);
-        stringTwoPrefixOnwards = firstHash.Substring(stringTwoPrefixIndex + 1);
+        var stringOnePrefixOnwards = firstHash.Substring(stringOnePrefixIndex + 1);
+        var stringTwoPrefixOnwards = firstHash.Substring(stringTwoPrefixIndex + 1);
 
         // Make sure there's something there
         if (string.IsNullOrEmpty(stringOnePrefixOnwards) || string.IsNullOrEmpty(stringTwoPrefixOnwards))
@@ -51,28 +53,27 @@ internal static class Comparisons
         // If a spamSum doesn't have two parts past the prefix, it's malformed and must be returned.
 
         var tempSplit = stringOnePrefixOnwards.Split(':');
-        stringOneBlockOne = tempSplit[0];
+        var stringOneBlockOne = tempSplit[0];
         if (tempSplit.Length == 1 || string.IsNullOrEmpty(tempSplit[1]))
             return -1;
-        stringOneBlockTwo = tempSplit[1].Split(',')[0];
+        var stringOneBlockTwo = tempSplit[1].Split(',')[0];
         tempSplit = stringTwoPrefixOnwards.Split(':');
-        stringTwoBlockOne = tempSplit[0];
+        var stringTwoBlockOne = tempSplit[0];
         if (tempSplit.Length == 1 || string.IsNullOrEmpty(tempSplit[1]))
             return -1;
-        stringTwoBlockTwo = tempSplit[1].Split(',')[0];
+        var stringTwoBlockTwo = tempSplit[1].Split(',')[0];
 
         // The comments for fuzzy_compare say to "Eliminate any sequences [of the same character] longer than 3".
         // What this actually means is that any sequences of the same character longer than 3 need to be reduced to size 3,
         // i.e. "9AgX87HAAAAAOKG5/Dqj3C2o/jlqW7Yn/nmcwlcKCwA9aJo9FcAKwf" becomes "9AgX87HAAAOKG5/Dqj3C2o/jlqW7Yn/nmcwlcKCwA9aJo9FcAKwf"
         // The reason for doing this is that these sequences contain very little info, so cutting them down helps with
         // part of scoring the strings later.
-
         Regex r = new Regex("(.)(?<=\\1\\1\\1\\1)", RegexOptions.Compiled);
 
-        stringOneBlockOne = r.Replace(stringOneBlockOne, String.Empty);
-        stringOneBlockTwo = r.Replace(stringOneBlockTwo, String.Empty);
-        stringTwoBlockOne = r.Replace(stringTwoBlockOne, String.Empty);
-        stringTwoBlockTwo = r.Replace(stringTwoBlockTwo, String.Empty);
+        stringOneBlockOne = r.Replace(stringOneBlockOne, string.Empty);
+        stringOneBlockTwo = r.Replace(stringOneBlockTwo, string.Empty);
+        stringTwoBlockOne = r.Replace(stringTwoBlockOne, string.Empty);
+        stringTwoBlockTwo = r.Replace(stringTwoBlockTwo, string.Empty);
 
 
         // Return 100 immediately if both spamSums are identical.
@@ -81,135 +82,124 @@ internal static class Comparisons
             if (stringOneBlockOne == stringTwoBlockOne && stringOneBlockTwo == stringTwoBlockTwo)
                 return 100;
 
-        // each signature has a string for two block sizes. We now
-        // choose how to combine the two block sizes. We checked above
-        // that they have at least one block size in common
+        // Choose different scoring combinations depending on block sizes present.
+        uint score;
         if (blockSizeOne <= uint.MaxValue / 2)
         {
             if (blockSizeOne == blockSizeTwo)
             {
-                uint score1, score2;
-                score1 = ScoreStrings(stringOneBlockOne, stringTwoBlockOne, blockSizeOne);
-                score2 = ScoreStrings(stringOneBlockTwo, stringTwoBlockTwo, blockSizeOne * 2);
+                var score1 = ScoreStrings(stringOneBlockOne, stringTwoBlockOne, blockSizeOne);
+                var score2 = ScoreStrings(stringOneBlockTwo, stringTwoBlockTwo, blockSizeOne * 2);
                 score = Math.Max(score1, score2);
             }
             else if (blockSizeOne * 2 == blockSizeTwo)
-            {
                 score = ScoreStrings(stringTwoBlockOne, stringOneBlockTwo, blockSizeTwo);
-            }
             else
-            {
                 score = ScoreStrings(stringOneBlockOne, stringTwoBlockTwo, blockSizeOne);
-            }
         }
         else
         {
             if (blockSizeOne == blockSizeTwo)
-            {
                 score = ScoreStrings(stringOneBlockOne, stringTwoBlockOne, blockSizeOne);
-            }
             else if (blockSizeOne % 2 == 0 && blockSizeOne / 2 == blockSizeTwo)
-            {
                 score = ScoreStrings(stringOneBlockOne, stringTwoBlockTwo, blockSizeOne);
-            }
             else
-            {
                 score = 0;
-            }
         }
 
         return (int)score;
     }
 
+    /// <summary>
+    /// Checks whether the two SpamSum strings have a common substring of 7 or more characters (as defined in fuzzy_compare's ROLLING_WINDOW size).
+    /// </summary>
+    /// <param name="stringOne">First string to score</param>
+    /// <param name="stringTwo">Second string to score</param>
+    /// <returns>False if there is no common substring of 7 or more characters, true if there is.</returns>
 
     private static bool HasCommmonSubstring(string stringOne, string stringTwo)
     {
-        int stringOneLength = stringOne.Length;
-        int stringTwoLength = stringTwo.Length;
+        var stringOneLength = stringOne.Length;
+        var stringTwoLength = stringTwo.Length;
+        var largestSubstring = 0;
 
-        int largestSubstring = 0;
-
-        for (int i = 0; i < stringOneLength; i++)
-        {
-            for (int j = 0; j < stringTwoLength; j++)
+        for (var i = 0; i < stringOneLength; i++)
+            for (var j = 0; j < stringTwoLength; j++)
             {
-                int curr = 0;
-                while ((i + curr) < stringOneLength && (j + curr) < stringTwoLength
-                                                    && stringOne[i + curr] == stringTwo[j + curr])
-                {
-                    curr++;
-                }
+                var currentIndex = 0;
+                while ((i + currentIndex) < stringOneLength && (j + currentIndex) < stringTwoLength && stringOne[i + currentIndex] == stringTwo[j + currentIndex])
+                    currentIndex++;
 
-                largestSubstring = Math.Max(largestSubstring, curr);
+                largestSubstring = Math.Max(largestSubstring, currentIndex);
             }
-        }
 
         if (largestSubstring >= 7)
             return true;
-        else
-            return false;
+        
+        return false;
     }
 
+    /// <summary>
+    /// Compares how similar two SpamSums are to each other. Implements ssdeep's fuzzy_compare.
+    /// </summary>
+    /// <param name="stringOne">First string to score</param>
+    /// <param name="stringTwo">Second string to score</param>
+    /// <param name="blockSize">Current blocksize</param>
+    /// <returns>-1 on validity failure, 0 if they're not comparable, score from 0 (least similar) to 100 (most similar) otherwise.</returns>
     private static uint ScoreStrings(string stringOne, string stringTwo, uint blockSize)
     {
         if (!HasCommmonSubstring(stringOne, stringTwo))
             return 0;
 
-        uint EDIT_DISTN_MAXLEN = 64;
-        uint EDIT_DISTN_INSERT_COST = 1;
-        uint EDIT_DISTN_REMOVE_COST = 1;
-        uint EDIT_DISTN_REPLACE_COST = 2;
+        const uint maxLength = 64;
+        const uint insertCost = 1;
+        const uint removeCost = 1;
+        const uint replaceCost = 2;
 
-        var t1 = new uint[EDIT_DISTN_MAXLEN + 1];
-        var t2 = new uint[EDIT_DISTN_MAXLEN + 1];
-        var t3 = new uint[EDIT_DISTN_MAXLEN + 1];
+        var traverseOne = new uint[maxLength + 1];
+        var traverseTwo = new uint[maxLength + 1];
+        uint[] tempArray;
 
-        uint i1, i2;
-        for (i2 = 0; i2 <= stringTwo.Length; i2++)
-            t1[i2] = i2 * EDIT_DISTN_REMOVE_COST;
-        for (i1 = 0; i1 < stringOne.Length; i1++)
+        uint indexOne, indexTwo;
+        for (indexTwo = 0; indexTwo <= stringTwo.Length; indexTwo++)
+            traverseOne[indexTwo] = indexTwo * removeCost;
+        for (indexOne = 0; indexOne < stringOne.Length; indexOne++)
         {
-            t2[0] = (i1 + 1) * EDIT_DISTN_INSERT_COST;
-            for (i2 = 0; i2 < stringTwo.Length; i2++)
+            traverseTwo[0] = (indexOne + 1) * insertCost;
+            for (indexTwo = 0; indexTwo < stringTwo.Length; indexTwo++)
             {
-                uint cost_a = t1[i2 + 1] + EDIT_DISTN_INSERT_COST;
-                uint cost_d = t2[i2] + EDIT_DISTN_REMOVE_COST;
-                uint cost_r = t1[i2] + (stringOne[(int)i1] == stringTwo[(int)i2] ? 0 : EDIT_DISTN_REPLACE_COST);
-                t2[i2 + 1] = Math.Min(Math.Min(cost_a, cost_d), cost_r);
+                var costA = traverseOne[indexTwo + 1] + insertCost;
+                var costD = traverseTwo[indexTwo] + removeCost;
+                var costR = traverseOne[indexTwo] + (stringOne[(int)indexOne] == stringTwo[(int)indexTwo] ? 0 : replaceCost);
+                traverseTwo[indexTwo + 1] = Math.Min(Math.Min(costA, costD), costR);
             }
 
-            t3 = t1;
-            t1 = t2;
-            t2 = t3;
+            tempArray = traverseOne;
+            traverseOne = traverseTwo;
+            traverseTwo = tempArray;
         }
 
-        long score = t1[stringTwo.Length];
+        long score = traverseOne[stringTwo.Length];
 
         const int spamSumLength = 64;
         const int rollingWindow = 7;
         const int minBlocksize = 3;
         score = (score * spamSumLength) / (stringOne.Length + stringTwo.Length);
 
-        // at this stage the score occurs roughly on a 0-SPAMSUM_LENGTH scale,
-        // with 0 being a good match and SPAMSUM_LENGTH being a complete
-        // mismatch
+        // Currently, the score ranges from 0-64 (64 being the length of a spamsum), with 0 being the strongest match
+        // and 64 being the weakest match.
 
-        // rescale to a 0-100 scale (friendlier to humans)
+        // Change scale to 0-100
         score = (100 * score) / spamSumLength;
 
-        // now re-scale on a 0-100 scale with 0 being a poor match and
-        // 100 being a excellent match.
+        // Invert scale so 0 is the weakest possible match and 100 is the strongest
         score = 100 - score;
-
-        //  printf ("s1len: %"PRIu32"  s2len: %"PRIu32"\n", (uint32_t)s1len, (uint32_t)s2len);
-
-        // when the blocksize is small we don't want to exaggerate the match size
+        
+        // Compensate for small blocksizes, so match isn't reported as overly strong.
         if (blockSize >= (99 + rollingWindow) / rollingWindow * minBlocksize)
             return (uint)score;
         if (score > blockSize / minBlocksize * Math.Min(stringOne.Length, stringTwo.Length))
-        {
             score = blockSize / minBlocksize * Math.Min(stringOne.Length, stringTwo.Length);
-        }
 
         return (uint)score;
     }
